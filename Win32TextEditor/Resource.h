fa65dec74ddb06cd55a26e553aee0434af0f2293
Darkkid819 <darkkid8192004@gmail.com>
2024-10-30 00:38:20 -0400

    added more file menu options, most unimplemented

--- Win32TextEditor/Resource.h
+++ Win32TextEditor/Resource.h
@@ -4,18 +4,44 @@
 #define IDI_WIN32TEXTEDITOR   101
 #define IDI_SMALL             102
 
-// Menu Commands
-#define ID_FILE_OPEN          1
-#define ID_FILE_SAVE          2
-#define IDM_EXIT              3
-#define IDM_ABOUT             4
+// File Menu Commands
+#define ID_FILE_NEW           201
+#define ID_FILE_OPEN          202
+#define ID_FILE_SAVE          203
+#define ID_FILE_SAVE_AS       204
+#define ID_FILE_CLOSE         205
+#define ID_FILE_EXIT          206
+
+// Edit Menu Commands
+#define ID_EDIT_UNDO          301
+#define ID_EDIT_REDO          302
+#define ID_EDIT_CUT           303
+#define ID_EDIT_COPY          304
+#define ID_EDIT_PASTE         305
+#define ID_EDIT_SELECT_ALL    306
+
+// View Menu Commands
+#define ID_VIEW_ZOOM_IN       401
+#define ID_VIEW_ZOOM_OUT      402
+#define ID_VIEW_RESET_ZOOM    403
+#define ID_VIEW_STATUS_BAR    404
+
+// Window Menu Commands
+#define ID_WINDOW_MINIMIZE    501
+#define ID_WINDOW_MAXIMIZE    502
+#define ID_WINDOW_CLOSE       503
+
+// Help Menu Commands
+#define ID_HELP_VIEW_HELP     601
+#define ID_HELP_CHECK_UPDATES 602
+#define ID_HELP_ABOUT         603
 
 // Dialogs
 #define IDD_ABOUTBOX          103
 
+// Editor Control ID
+#define IDC_EDITOR            200
+
 // String Table Identifiers
 #define IDS_APP_TITLE         104
 #define IDC_WIN32TEXTEDITOR   105
-
-// Editor Control ID 
-#define IDC_EDITOR            200
--- Win32TextEditor/menu.cpp
+++ Win32TextEditor/menu.cpp
@@ -5,21 +5,65 @@
 
 void Menu::initialize(HWND hwnd) {
     hMenu = CreateMenu();
-    HMENU hFileMenu = CreatePopupMenu();
 
+    // File Menu
+    HMENU hFileMenu = CreatePopupMenu();
+    AppendMenu(hFileMenu, MF_STRING, ID_FILE_NEW, L"New");
     AppendMenu(hFileMenu, MF_STRING, ID_FILE_OPEN, L"Open");
     AppendMenu(hFileMenu, MF_STRING, ID_FILE_SAVE, L"Save");
-    AppendMenu(hFileMenu, MF_STRING, IDM_EXIT, L"Exit");
-    AppendMenu(hFileMenu, MF_STRING, IDM_ABOUT, L"About");
+    AppendMenu(hFileMenu, MF_STRING, ID_FILE_SAVE_AS, L"Save As");
+    AppendMenu(hFileMenu, MF_STRING, ID_FILE_CLOSE, L"Close");
+    AppendMenu(hFileMenu, MF_SEPARATOR, 0, NULL);
+    AppendMenu(hFileMenu, MF_STRING, ID_FILE_EXIT, L"Exit");
+
+    // Edit Menu
+    HMENU hEditMenu = CreatePopupMenu();
+    AppendMenu(hEditMenu, MF_STRING, ID_EDIT_UNDO, L"Undo");
+    AppendMenu(hEditMenu, MF_STRING, ID_EDIT_REDO, L"Redo");
+    AppendMenu(hEditMenu, MF_SEPARATOR, 0, NULL);
+    AppendMenu(hEditMenu, MF_STRING, ID_EDIT_CUT, L"Cut");
+    AppendMenu(hEditMenu, MF_STRING, ID_EDIT_COPY, L"Copy");
+    AppendMenu(hEditMenu, MF_STRING, ID_EDIT_PASTE, L"Paste");
+    AppendMenu(hEditMenu, MF_STRING, ID_EDIT_SELECT_ALL, L"Select All");
+
+    // View Menu
+    HMENU hViewMenu = CreatePopupMenu();
+    AppendMenu(hViewMenu, MF_STRING, ID_VIEW_ZOOM_IN, L"Zoom In");
+    AppendMenu(hViewMenu, MF_STRING, ID_VIEW_ZOOM_OUT, L"Zoom Out");
+    AppendMenu(hViewMenu, MF_STRING, ID_VIEW_RESET_ZOOM, L"Reset Zoom");
+    AppendMenu(hViewMenu, MF_STRING, ID_VIEW_STATUS_BAR, L"Toggle Status Bar");
+
+    // Window Menu
+    HMENU hWindowMenu = CreatePopupMenu();
+    AppendMenu(hWindowMenu, MF_STRING, ID_WINDOW_MINIMIZE, L"Minimize");
+    AppendMenu(hWindowMenu, MF_STRING, ID_WINDOW_MAXIMIZE, L"Maximize");
+    AppendMenu(hWindowMenu, MF_STRING, ID_WINDOW_CLOSE, L"Close Window");
+
+    // Help Menu
+    HMENU hHelpMenu = CreatePopupMenu();
+    AppendMenu(hHelpMenu, MF_STRING, ID_HELP_VIEW_HELP, L"View Help");
+    AppendMenu(hHelpMenu, MF_STRING, ID_HELP_CHECK_UPDATES, L"Check for Updates");
+    AppendMenu(hHelpMenu, MF_STRING, ID_HELP_ABOUT, L"About");
 
     AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hEditMenu, L"Edit");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hViewMenu, L"View");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hWindowMenu, L"Window");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hHelpMenu, L"Help");
+
     SetMenu(hwnd, hMenu);
 }
+
 
 void Menu::handleMenuCommand(WPARAM wParam) {
     App& app = App::getInstance();
 
     switch (LOWORD(wParam)) {
+    case ID_FILE_NEW:
+        app.getEditor().clear();
+        break;
+
+    // File Menu
     case ID_FILE_OPEN: {
         std::wstring path = openFileDialog(app.getMainWindow());
         if (!path.empty()) {
@@ -36,11 +80,67 @@ void Menu::handleMenuCommand(WPARAM wParam) {
         }
         break;
     }
-    case IDM_EXIT:
+
+    case ID_FILE_SAVE_AS:
+        break;
+
+    case ID_FILE_CLOSE:
+        break;
+
+    case ID_FILE_EXIT:
         PostQuitMessage(0);
         break;
+
+	// Edit Menu
+	case ID_EDIT_UNDO:
+		break;
+
+	case ID_EDIT_REDO:
+        break;
+
+	case ID_EDIT_CUT:
+        break;
+
+	case ID_EDIT_COPY:
+        break;
+
+	case ID_EDIT_PASTE:
+        break;
+
+	case ID_EDIT_SELECT_ALL:
+        break;
+
+	// View Menu
+	case ID_VIEW_ZOOM_IN:
+		break;
+
+	case ID_VIEW_ZOOM_OUT:
+		break;
+
+	case ID_VIEW_RESET_ZOOM:
+		break;
+
+	case ID_VIEW_STATUS_BAR:
+		break;
+
+	// Window Menu
+	case ID_WINDOW_MINIMIZE:
+		break;
+
+	case ID_WINDOW_MAXIMIZE:
+		break;
+
+	case ID_WINDOW_CLOSE:
+		break;
     
-    case IDM_ABOUT:
+    // Help Menu
+	case ID_HELP_VIEW_HELP:
+		break;
+
+	case ID_HELP_CHECK_UPDATES:
+		break;
+
+    case ID_HELP_ABOUT:
         DialogBox(
             GetModuleHandle(NULL),
             MAKEINTRESOURCE(IDD_ABOUTBOX),
